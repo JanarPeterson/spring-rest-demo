4c50acdac68dd2cb9aebb6f6aeb7eb052c50d506
janarpeterson <jenku18@gmail.com>
2019-01-30 13:16:08 +0200

    adding getStudentById mapping and created postconstruct method with needed dependency.

--- src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -2,9 +2,11 @@ package com.luv2code.springdemo.rest;
 
 import com.luv2code.springdemo.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,14 +14,25 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
+
+   private List<Student> students;
+
+   @PostConstruct
+   public void loadData(){
+       students = new ArrayList<>();
+       students.add(new Student("Mati", "Karu"));
+       students.add(new Student("Kati", "Karu"));
+       students.add(new Student("Jaan", "kivi"));
+   }
+
     @GetMapping("/students")
     public List<Student> getAllStudents(){
-        List<Student> students = new ArrayList<>();
-
-        students.add(new Student("Mati", "Karu"));
-        students.add(new Student("Kati", "Karu"));
-        students.add(new Student("Jaan", "kivi"));
-
         return students;
+    }
+
+
+    @GetMapping("/students/{id}")
+    public Student getAllStudentById(@PathVariable("id")int id){
+        return students.get(id);
     }
 }
